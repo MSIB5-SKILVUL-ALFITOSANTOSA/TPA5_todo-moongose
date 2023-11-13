78736db4178e341ff6cd1d8b038e0e950210c94f
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-11-13 11:00:15 +0700

    fix update todo

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const route = express.Router();
 
+//mengumpulkan semua route
 const userRoute = require("./user-route");
 const todoRoute = require("./todo-route");
 const authRoute = require("./auth-route");
 
+//route index
 route.get("/", (req, res) => {
   res.json("ini dari express mongoose dari route index.js");
 });
 
+//route lainnya
 route.use("/auth", authRoute);
 route.use("/users", userRoute);
 route.use("/todos", todoRoute);
-// route.use("/user", userRoute)
 
 module.exports = route;
