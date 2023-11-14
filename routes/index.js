e2f75e9958921e42b474a0b232de9541e0d6c914
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-11-14 11:36:21 +0700

    push index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const UserRoute = require('./user_route')
-const todoRoute = require('./todo_route')
-const authRoute = require('./auth_route')
+const UserRoute = require("./user_route");
+const todoRoute = require("./todo_route");
+const authRoute = require("./auth_route");
 
 router.get("/", (req, res) => {
-    res.json("INI DARI MASA DEPAN")
-})
+  res.json("selamat datang di API todo-list IG : @alfitosans");
+});
 
 router.use("/auth", authRoute);
 router.use("/users", UserRoute);
 router.use("/todos", todoRoute);
-
 
 module.exports = router;
