43d65f53c8af1f0855e000b9fe73da2c03614d27
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-11-14 12:36:56 +0700

    fixing routes

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,33 +1,35 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+require("dotenv").config();
 
 module.exports = {
-    login: async (req, res) => {
+  login: async (req, res) => {
+    try {
+      const userLogin = req.body;
 
-        try {
-            const userLogin = req.body;
+      const user = await User.findOne({ email: userLogin.email });
+      if (!user) throw new Error("Email not found");
 
-            const user = await User.findOne({ email: userLogin.email });
-            if (!user) throw new Error("Email not found");
+      if (userLogin.password !== user.password)
+        throw new Error("Password not match");
 
-            if (userLogin.password !== user.password) throw new Error("Password not match");
-
-            const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET);
-            res.json({
-                message: "success login",
-                token: token,
-                userID: user._id
-            });
-        } catch (error) {
-            res.json({
-                message: error.message
-            });
-        }
-
-    },
-
-    registes: async (req, res) => {
-        res.json("register")
+      const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET
+      );
+      res.json({
+        message: "success login",
+        token: token,
+        userID: user._id,
+      });
+    } catch (error) {
+      res.json({
+        message: error.message,
+      });
     }
-}
+  },
+
+  register: async (req, res) => {
+    res.json("register");
+  },
+};
--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { login, registes } = require('../controller/auth_controller');
+const express = require("express");
+const { login, register } = require("../controller/auth_controller");
 const router = express.Router();
 
-router.post("/login", login)
-router.post("/register", registes)
+router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
